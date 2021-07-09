66ee90bfda334aa5ea88c9cdebf4c889d1635651
chanijung <1016chani@kaist.ac.kr>
2021-07-09 09:10:36 +0000

    Organized code measuring bias

--- notebooks/bert_expose_bias_with_prior.py
+++ notebooks/bert_expose_bias_with_prior.py
@@ -9,9 +9,15 @@ import numpy as np
 from pathlib import Path
 from typing import *
 import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
-
+import torch.nn as nn
 import time
+# get_ipython().run_line_magic('matplotlib', 'inline')
+
+# get_ipython().run_line_magic('load_ext', 'autoreload')
+# get_ipython().run_line_magic('autoreload', '2')
+
+import sys
+sys.path.append("../lib")
 
 
 # In[2]:
@@ -54,6 +60,11 @@ def spacy_tok(s: str):
     return [w.text for w in nlp(s)]
 
 
+def softmax(arr, axis=1):
+    e = np.exp(arr)
+    return e / e.sum(axis=axis, keepdims=True)
+
+
 # In[5]:
 
 
@@ -75,7 +86,6 @@ def tokenize(x: str) -> List[Token]:
         )[:config.max_seq_len]]
 
 
-# In[6]:
 
 
 from pytorch_pretrained_bert import BertConfig, BertForMaskedLM
@@ -87,19 +97,36 @@ from allennlp.common import Params
 # model = BertForMaskedLM.from_pretrained(config.model_type)
 
 #KnowBERT
-archive_file = 'debiased_wordnet/model.tar.gz'
+archive_file = 'models/kb/wordnet/pure/e1_s240000/model.tar.gz'
+print(f'archive: {archive_file}')
 params = Params({"archive_file": archive_file})
 # load model and batcher
+before=time.time()
 model = ModelArchiveFromParams.from_params(params=params)
-device = list(range(torch.cuda.device_count()))[0]
-model.cuda()
-model.eval()
+print(f'{time.time()-before} sec taken until model')
+# device = list(range(torch.cuda.device_count()))[0]
+# model.cuda()
 for p in model.parameters():
     p.requires_grad_(False)
 print("model built!!!!!!!")
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# if torch.cuda.device_count() > 1:
+#   print("Let's use", torch.cuda.device_count(), "GPUs!")
+  # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+  # model = nn.DataParallel(model)
+
+model.to(device)
+model.eval()
+
 batcher = KnowBertBatchifier(archive_file, masking_strategy='full_mask')
-mask_id = batcher.tokenizer_and_candidate_generator.bert_tokenizer.vocab['[MASK]']
+print(f'{time.time()-before} sec taken until batcher')
+vocab = batcher.tokenizer_and_candidate_generator.bert_tokenizer.vocab
+print(f'{time.time()-before} sec taken until vocab')
+# mask_id = batcher.tokenizer_and_candidate_generator.bert_tokenizer.vocab['[MASK]']
+mask_id = vocab['[MASK]']
+
 # if torch.cuda.is_available():
 #     cuda_device = list(range(torch.cuda.device_count()))
 #     model = model.cuda(cuda_device[0])
@@ -119,39 +146,6 @@ from allennlp.data import Vocabulary
 # token_indexer._add_encoding_to_vocabulary(vocab)
 
 
-# In[8]:
-
-
-# def get_logits(input_sentence: str) -> torch.Tensor:
-#     input_toks = tokenize(input_sentence)
-#     batch = token_indexer.tokens_to_indices(input_toks, vocab, "tokens")
-#     token_ids = torch.LongTensor(batch["tokens"]).unsqueeze(0)
-#     with torch.no_grad():
-#         out_logits = model(token_ids).squeeze(0)
-#     return out_logits.detach().cpu().numpy()
-
-
-# In[9]:
-
-
-# full_vocab = {v:k for k, v in token_indexer.vocab.items()}
-
-# def indices_to_words(indices: Iterable[int]) -> List[str]:
-#     return [full_vocab[x] for x in indices]
-
-
-
-
-
-
-
-# Measuring difference
-
-# In[17]:
-
-
-# male_logits = get_logits("he is very [MASK].")[4, :]
-# female_logits = get_logits("she is very [MASK].")[4, :]
 
 
 # In[18]:
@@ -162,86 +156,11 @@ def softmax(x, axis=0, eps=1e-9):
     return e / (e.sum(axis, keepdims=True) + eps)
 
 
-# In[19]:
-
-
-# male_probs = softmax(male_logits)
-# female_probs = softmax(female_logits)
-
-
-# In[20]:
-
-
-# msk = ((male_probs >= 1e-6) & (female_probs >= 1e-6))
-# male_probs = male_probs[msk]
-# female_probs = female_probs[msk]
-
-
-# In[21]:
-
-
-# [(pos + 1, full_vocab[i]) for i, pos in enumerate((male_probs / female_probs).argsort()) if pos < 10]
-
-
-# In[22]:
-
-
-# [(pos + 1, full_vocab[i]) for i, pos in enumerate((female_probs / male_probs).argsort()) if pos < 10]
-
-
-# In[ ]:
-
-
-
-
-
-# # Construct measure of bias
-
-# In[23]:
-
-
-# input_sentence = "[MASK] is intelligent"
-
-
-# In[24]:
-
-
-# def _get_mask_index(toks: Iterable[Token]) -> int:
-#     for i, t in enumerate(toks):
-#         if t.text == "[MASK]":
-#             return i + 1 # take the [CLS] token into account
-#     raise ValueError("No [MASK] token found")
-
-
-# In[25]:
-
-
-# def get_logits(input_sentence: str, n_calc: int=10) -> np.ndarray:
-#     """
-#     n_calc: Since the logits are non-deterministic, 
-#     computing the logits multiple times might be better
-#     """
-#     input_toks = tokenize(input_sentence)
-#     batch = token_indexer.tokens_to_indices(input_toks, vocab, "tokens")
-#     token_ids = torch.LongTensor(batch["tokens"]).unsqueeze(0)
-    
-#     logits = None
-#     for _ in range(n_calc):
-#         with torch.no_grad():
-#             out_logits = model(token_ids).squeeze(0)
-#         if logits is None: logits = np.zeros(out_logits.shape)
-#         logits += out_logits.detach().cpu().numpy()
-#     return logits / n_calc
-
-
-# In[26]:
-
 
 def get_logit_scores(input_sentence: str, words: int) -> Dict[str, float]:
     # out_logits = get_logits(input_sentence)
     # input_toks = tokenize(input_sentence)
     # i = _get_mask_index(input_toks)
-    start = time.time()
 
     sentences = [input_sentence]
     # print(f'sentences: {sentences}')
@@ -254,8 +173,12 @@ def get_logit_scores(input_sentence: str, words: int) -> Dict[str, float]:
     # batcher takes raw untokenized sentences
     # and yields batches of tensors needed to run KnowBert
     # print(f'created batcher, mask_id {time.time()-start}')
+    # device = list(range(torch.cuda.device_count()))[0]
+    print(f'Inside get_log_odds')
+    start = time.time()
     with torch.no_grad():
         for batch in batcher.iter_batches(sentences):
+            print(f'{time.time()-start} sec taken until iter_batches')
             # batch = {k:v.type(torch.long).to(device) for k,v in batch.items()}
             # print(f'batch\n{batch}')
             batch_ = move_to(batch, device)
@@ -276,7 +199,7 @@ def get_logit_scores(input_sentence: str, words: int) -> Dict[str, float]:
 
     mask_token_probabilities = mask_token_probabilities.detach().cpu().numpy()
     # return {w: out_logits[i, token_indexer.vocab[w]] for w in words}
-    vocab = batcher.tokenizer_and_candidate_generator.bert_tokenizer.vocab
+    
     # print(f'before return {time.time()-start}')
     # for w in words:
         # print(f'vocab index for {w}: {vocab[w]}')
@@ -285,6 +208,96 @@ def get_logit_scores(input_sentence: str, words: int) -> Dict[str, float]:
 def get_log_odds(input_sentence: str, word1: str, word2: str) -> float:
     scores = get_logit_scores(input_sentence, (word1, word2))
     return scores[word1] - scores[word2]
+
+def get_mask_fill_logits(sentence: str, words: Iterable[str],
+                         use_last_mask=False, apply_softmax=False) -> Dict[str, float]:
+    # mask_i = processor.get_index(sentence, "[MASK]", last=use_last_mask)
+    # logits = defaultdict(list)
+    # out_logits = get_logits(sentence)
+    # if apply_softmax: 
+    #     out_logits = softmax(out_logits)
+    # return {w: out_logits[mask_i, processor.token_to_index(w)] for w in words}
+
+    sentences = [sentence]
+    print(f'sentences {sentences}')
+
+
+    # batcher takes raw untokenized sentences
+    # and yields batches of tensors needed to run KnowBert
+    for batch in batcher.iter_batches(sentences):
+        batch_ = move_to(batch, device)
+        model_output = model(**batch_)
+        
+        token_mask = batch_['tokens']['tokens'] == mask_id
+
+        # (batch_size, timesteps, vocab_size)
+        prediction_scores, _ = model.pretraining_heads(
+                model_output['contextual_embeddings'], model_output['pooled_output']
+        )
+        print(f'pred score size {prediction_scores.size()}')
+
+        if apply_softmax: 
+            prediction_scores = softmax(prediction_scores)
+
+        # (num_masked_tokens, vocab_size)
+        mask_token_probabilities = prediction_scores.masked_select(token_mask.unsqueeze(-1)).view(-1, prediction_scores.shape[-1])  # (num_masked_tokens, vocab_size)
+        if use_last_mask:
+          mask_token_probabilities = mask_token_probabilities[1, :]
+        else:
+          mask_token_probabilities = mask_token_probabilities[0, :]
+        print(f'mask_token_prob size {mask_token_probabilities.size()}')
+
+    for w in words:
+        print(f'vocab index for {w}: {vocab[w]}')
+    return {w: mask_token_probabilities[vocab[w]] for w in words}
+
+
+def bias_score(sentence: str, gender_words: Iterable[str], 
+               word: str, gender_comes_first=True) -> Dict[str, float]:
+    """
+    Input a sentence of the form "GGG is XXX"
+    XXX is a placeholder for the target word
+    GGG is a placeholder for the gendered words (the subject)
+    We will predict the bias when filling in the gendered words and 
+    filling in the target word.
+    
+    gender_comes_first: whether GGG comes before XXX (TODO: better way of handling this?)
+    """
+    # probability of filling [MASK] with "he" vs. "she" when target is "programmer"
+    mw, fw = gender_words
+    subject_fill_logits = get_mask_fill_logits(
+        sentence.replace("XXX", word).replace("GGG", "[MASK]"), 
+        gender_words, use_last_mask=not gender_comes_first,
+    )
+    subject_fill_bias = subject_fill_logits[mw] - subject_fill_logits[fw]
+    # male words are simply more likely than female words
+    # correct for this by masking the target word and measuring the prior probabilities
+    subject_fill_prior_logits = get_mask_fill_logits(
+        sentence.replace("XXX", "[MASK]").replace("GGG", "[MASK]"), 
+        gender_words, use_last_mask=gender_comes_first,
+    )
+    subject_fill_bias_prior_correction = subject_fill_prior_logits[mw] - subject_fill_prior_logits[fw]
+    
+    # probability of filling "programmer" into [MASK] when subject is male/female
+    try:
+        mw_fill_prob = get_mask_fill_logits(
+            sentence.replace("GGG", mw).replace("XXX", "[MASK]"), [word],
+            apply_softmax=True,
+        )[word]
+        fw_fill_prob = get_mask_fill_logits(
+            sentence.replace("GGG", fw).replace("XXX", "[MASK]"), [word],
+            apply_softmax=True,
+        )[word]
+        # We don't need to correct for the prior probability here since the probability
+        # should already be conditioned on the presence of the word in question
+        tgt_fill_bias = np.log(mw_fill_prob / fw_fill_prob)
+    except:
+        tgt_fill_bias = np.nan # TODO: handle multi word case
+    return {"gender_fill_bias": subject_fill_bias,
+            "gender_fill_prior_correction": subject_fill_bias_prior_correction,
+            "gender_fill_bias_prior_corrected": subject_fill_bias - subject_fill_bias_prior_correction,
+            "target_fill_bias": tgt_fill_bias, 
+           }
 
 
 def move_to(obj, device):
@@ -303,3 +316,9 @@ def move_to(obj, device):
   else:
     raise TypeError("Invalid type for move_to")
 
+if __name__== '__main__':
+  ans = bias_score("GGG is XXX", ["he", "she"], 'good at ')
+  score= ans['gender_fill_bias_prior_corrected']
+  print(f'score {score}')
+  print(f'score.size() {scroe.size()}')
+  print(f'score[0] {score[0]}')
--- notebooks/exposing_bias_bert.py
+++ notebooks/exposing_bias_bert.py
@@ -20,10 +20,13 @@ sys.path.append("../lib")
 from bert_utils import Config, BertPreprocessor
 
 from bert_expose_bias_with_prior import *
-from construct_bias_score import *
+# from construct_bias_score import *
 
 from tqdm import tqdm
 import time
+import os
+
+
 
 def Txt2List(file):
     ll=[]
@@ -31,7 +34,7 @@ def Txt2List(file):
         for line in f:
             ll.append(line.strip().lower().replace("_", " "))
     return ll
-        
+
 def plot_pie(file, mc=50, fc=50):
 #mc=50
 #fc=50
@@ -44,12 +47,16 @@ def plot_pie(file, mc=50, fc=50):
     fig = plt.figure()
     plt.pie(sizes, labels=labels, colors=colors,
     autopct='%1.1f%%', shadow=True, startangle=140)
-
     plt.axis('equal')
+
+    tmp = file.split('/')[-1]
+    fig.savefig(tmp)
+
     fig.savefig(file)
+
     plt.show()
     
-def list2Bias_norm(plotfile, var_list, abs_str, print_str): #Example of abs_str is "good at ", print_str is "is good at "
+def list2Bias_norm(output_dir, plotfile, var_list, abs_str, print_str): #Example of abs_str is "good at ", print_str is "is good at "
     mc=0
     fc=0
     for var in tqdm(var_list):
@@ -65,12 +72,10 @@ def list2Bias_norm(plotfile, var_list, abs_str, print_str): #Example of abs_str 
             fc+=1
             print("Woman ",print_str,  var, " by ", score)
 
-
-
-    plot_pie(plotfile, mc, fc)
+    plot_pie(os.path.join(output_dir, plotfile), mc, fc)
     
     
-def list2Bias(plotfile, var_list, abs_str): #Example of abs_str "is good at "
+def list2Bias(output_dir, plotfile, var_list, abs_str): #Example of abs_str "is good at "
     start = time.time()
     mc=0
     fc=0
@@ -87,20 +92,18 @@ def list2Bias(plotfile, var_list, abs_str): #Example of abs_str "is good at "
             # print("Woman ",abs_str,  var, " by ", score)
         
     print(f'mc {mc}, fc {fc}')
-    plot_pie(plotfile, mc, fc)
+    plot_pie(os.path.join(output_dir, plotfile), mc, fc)
     print(f'{(time.time()-start)/60} min spent')
+
+
+
 
 """# Exposing Bias in BERT
 
 
 In this notebook, I'll experiment with a couple of possibilities for exposing Bias in BERT. We will concentrate on gender bias for now and look at a possible extension for racial Bias.
-
-
 I am trying to look for ways of exposing bias that have a clear negative impact on the party against which the bias is present. But for each of these, we will need a good dataset (so, we might want to replace current datasets with larger/reliable/authoritative datasets in the future)
-
-
 Currently, I am using the Masked Prediction Task but we might be able to extend this for Next Sentence Prediction as well.
-
 We will classify the types of negative impact that are possible and look at experiments on their possible causes-
 
 
@@ -109,195 +112,39 @@ We will classify the types of negative impact that are possible and look at expe
 
 Employers may use searching/ranking based on certain skills or job titles . They might want to specifically look for certain skills, traits and impactful positions.
 
-
-### a) Bias for associating advanced skills with a  group - 
-
-Example-  "Cloud Computing", "Machine Learning" , "Deep Learning" , "Management" etc.
-
-#### Dataset: https://learning.linkedin.com/blog/top-skills/the-skills-companies-need-most-in-2019--and-how-to-learn-them
-
-#### 25X In-Demand Tech Skills according to Linkedin
-
-
-
-
-"""
-
-# #Load Dataset
-# skills = Txt2List('data/in_demand_tech_skills')
-
-# # Before Removing Prior
-
-# list2Bias('in_demand_tech_skills.pdf', skills, "is good at ")
-
-# # After Removing Prior
-
-# list2Bias_norm('in_demand_tech_skills_without_prior.pdf', skills, "good at ", "is good at ")
-
 """### b) Bias for associating positive traits with a group - 
 
-#### Dataset- http://ideonomy.mit.edu/essays/traits.html
-"""
 
-#Load Dataset
-print(f'--------positive traits---------')
-pos_traits_list = Txt2List('bin/debiasing_words/refined_positive_traits')
+if __name__ == '__main__':
+    import argparse
 
-list2Bias('positive_traits.pdf', pos_traits_list, "is ")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_dir', type=str)
+    args = parser.parse_args()
 
 
-# list2Bias_norm('positive_traits_without_prior.pdf', pos_traits_list, " ", "is ")
+    #Load Dataset
+    print(f'--------positive traits---------')
+    pos_traits_list = Txt2List('bin/debiasing_words/refined_positive_traits')
 
-"""### c) Bias for associating negative traits with a  group - """
+    list2Bias(args.output_dir,'positive_traits.pdf', pos_traits_list, "is ")
+    list2Bias_norm(args.output_dir, 'positive_traits_without_prior.pdf', pos_traits_list, " ", "is ")
 
-#Load Dataset
-print(f'--------negative traits---------')
-neg_traits_list = Txt2List('bin/debiasing_words/refined_negative_traits')
+    ### c) Bias for associating negative traits with a  group - """
 
-list2Bias('negative_traits.pdf', neg_traits_list, "is ")
+    #Load Dataset
+    print(f'--------negative traits---------')
+    neg_traits_list = Txt2List('bin/debiasing_words/refined_negative_traits')
 
-# list2Bias_norm('negative_traits_without_prior.pdf', neg_traits_list, " ", "is ")
+    list2Bias(args.output_dir, 'negative_traits.pdf', neg_traits_list, "is ")
+    list2Bias_norm(args.output_dir, 'negative_traits_without_prior.pdf', neg_traits_list, " ", "is ")
 
-"""### d) Bias for associating high salary jobs with a group - 
+    ### d) Bias for associating high salary jobs with a group - 
 
-#### Dataset- https://catalog.data.gov/dataset/employee-salaries-2017
-"""
+    print(f'--------top job titles---------')
+    Top_Titles = Txt2List('bin/debiasing_words/refined_job_titles.txt')
+    # print(f'top titles len {len(Top_Titles)}')
 
-#Load Dataset
-
-# Title=[]
-# Salary=[]
-# # flag=0
-# def isFloat(value):
-#     try:
-#         float(value)
-#         return True
-#     except ValueError:
-#         return False
-
-# with open('notebooks/data/employeesalaries2017.csv') as f:
-#     for line in f:
-#         if flag==0:
-#             flag=1
-#             continue
-#         row = line.split(',')
-#         Title.append(row[2])
-#         if isFloat(row[8]):
-#             Salary.append(float(row[8]))
-#         else:
-#             Title.pop()
-         
-        
-# Title_sorted = sorted(Title,key=dict(zip(Title, Salary)).get,reverse=True)
-
-# unique_titles= set()
-
-# Top_Titles= []
-
-
-# for i in Title_sorted:
-#     if i in unique_titles:
-#         continue
-#     else:
-#         Top_Titles.append(i.lower())
-#         unique_titles.add(i)
-print(f'--------top job titles---------')
-Top_Titles = Txt2List('bin/debiasing_words/refined_job_titles.txt')
-
-# print(f'top titles len {len(Top_Titles)}')
-
-list2Bias('TopTitles.pdf', Top_Titles, "is ")
-
-# list2Bias_norm('TopTitles_without_prior.pdf', Top_Titles, " ", "is ")
-
-"""### e) Associating skills sought by Google with a group
-
-#### Dataset: https://www.kaggle.com/niyamatalmass/google-job-skills
-"""
-
-
-
-
-
-
-
-
-
-"""### f) Associating skills sought by Amazon with a group
-
-#### Dataset: https://www.kaggle.com/atahmasb/amazon-job-skills
-"""
-
-
-
-
-
-
-
-"""### h) Associating skills sought by US based jobs (20k) on Dice.com
-
-#### Dataset: https://www.kaggle.com/PromptCloudHQ/usbased-jobs-from-dicecom
-"""
-
-
-
-
-
-
-
-"""
-# -----ROUGH------
-
-
-### b) Bias for associating impactful roles with a group-
-
-(Imperfect) Proxies for measuring impact- Salary, Prestige & Mixed (Based on dataset)
-
-2.1 Salary
-
-#### Datasets:
-
-https://www.careeronestop.org/Toolkit/Wages/highest-paying-careers.aspx
-
-2.2 Prestige: 
-
-#### Datasets:
-
-https://www.businessinsider.com/most-prestigious-jobs-in-america-2014-11
-
-
-
-
-2.3 Other methodologies
-
-Dataset: 
-
-
-
-
-3. Bias for associating professional traits with a group-
-
-#### Datasets:
-
-https://www.monster.ca/career-advice/article/50-personality-traits-for-the-workplace-canada
-
-https://learning.linkedin.com/blog/top-skills/the-skills-companies-need-most-in-2019--and-how-to-learn-them
-
-
-Social/Cultural Impact-
-
-1. Bias for associating negative traits (Eg- 'neurotic', 'weak') with a group
-
-2. Bias for associating certain life roles  (Eg- 'homemaker' , 'bread winner') with a group
-
-
-
-
-Some of this maybe extended to race as well-
-
-1. Bias for associating negative traits with a group
-
-2. Bias for associating profesional traits with a group
-
-"""
+    list2Bias(args.output_dir, 'highpaying_jobs.pdf', Top_Titles, "is ")
+    list2Bias_norm(args.output_dir, 'TopTitles_without_prior.pdf', Top_Titles, " ", "is ")
 
